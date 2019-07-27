6449039c07e04e23d8e1240a7ba37fef1dc437c5
kevcav91 <kevincavanau@gmail.com>
2019-07-27 11:26:56 -0700

    lesson5 fixes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -19,19 +19,20 @@ def get_fact():
 
 @app.route('/')
 def home():
+
     fact = get_fact().strip()
 
     url = "https://hidden-journey-62459.herokuapp.com/piglatinize/"
-    payload = {'input': fact}
-    resp = requests.post(url,data=payload, allow_redirects=False)
+    payload = {'input_text': fact}
+    resp = requests.post(url, data=payload, allow_redirects=False)
 
     template = f"""
-    <a hrefs={resp.headers['location']}>{resp.headers['location']}</a>
+    <a href={resp.headers['location']}>{resp.headers['location']}</a>
     """
+
     return template
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
